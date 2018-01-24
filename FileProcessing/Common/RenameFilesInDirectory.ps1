6dc11b899496796969d50a2714523e833902a418
Zach Frick <zach.frick@gmail.com>
2018-01-24 08:51:54 -0700

    Added extension qualifier setting

--- FileProcessing/Common/RenameFilesInDirectory.ps1
+++ FileProcessing/Common/RenameFilesInDirectory.ps1
@@ -1,7 +1,8 @@
 
 
 $dir = "C:\filedirectory"
+$extensionQualifier = ".txt"
 
+get-childitem -Path $dir -File | WHERE { $_.BaseName -like "prefix_*" -and $_.Extension -eq $extensionQualifier } | Rename-Item -NewName {$_.name -replace $extensionQualifier, ("_" + (get-date).ToString("yyyyMMddHHmm") + $extensionQualifier)}
 
-get-childitem -Path $dir -File | WHERE { $_.BaseName -like "prefix_*" -and $_.Extension -eq ".txt" } | Rename-Item -NewName {$_.name -replace ".txt", ("_" + (get-date).ToString("yyyyMMddHHmm") + ".txt")}
 
